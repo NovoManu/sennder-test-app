1d5d2f9e5604a64bc7f25e7cdf1a2f72a2ac2abf
NovoManu <operamike@mail.ru>
2020-04-25 19:32:39 +0300

    Initial setup

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    App
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import 'assets/scss/app';
+</style>
